54d59c42cfa837cea439f6093f7edc06405a5a15
Jack Gore <jgore@uwaterloo.ca>
2018-12-18 12:53:04 -0500

    Adding common errors to library

---
+++ errors.go
@@ -0,0 +1,8 @@
+package httputil
+
+const (
+	InternalServerError = "internal server error"
+	JSONParseError      = "unable to parse object as JSON"
+	MalformedRequest    = "malformed request body"
+)
+
