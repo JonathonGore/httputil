d8ac7c447c521d547e907cd918d13f728a3f8d32
Jack Gore <jgore@uwaterloo.ca>
2018-12-18 13:29:57 -0500

    added another error

--- errors.go
+++ errors.go
@@ -1,8 +1,8 @@
 package httputil
 
 const (
+	BadRequest          = "bad request"
 	InternalServerError = "internal server error"
 	JSONParseError      = "unable to parse object as JSON"
 	MalformedRequest    = "malformed request body"
 )
-
